cfc5e372a3eafb37d60b7f6b682a502f3187f20b
Thiago Costa <tigurrock@gmail.com>
2022-10-21 10:36:07 -0300

    att

---
+++ exe7/script.js
@@ -0,0 +1,8 @@
+let titulo = document.querySelector('h1')
+let instrucoes = document.querySelector('#instrucoes')
+let aviso = document.querySelector('#aviso')
+let pontos = 0
+let placar = 0
+
+let numQuestao = document.querySelector('#numQuestao')
+let pergunta   = document.querySelector('#pergunta')
