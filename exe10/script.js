1e0f2f2f98a80d7b00a06aeabc51043a96b4118a
Thiago Costa <tigurrock@gmail.com>
2022-11-13 10:27:09 -0300

    att

--- exe10/script.js
+++ exe10/script.js
@@ -6,45 +6,38 @@ let autoriaCitacao = citacao00.querySelector('span')
 let aviso = document.querySelector('#aviso')
 let divBaixar = document.querySelector('#divBaixar')
 
-function criarCitacao() {
-    let dataInput = document.querySelector('#data').value
-    let citacao   = document.querySelector('#citacao').value
-    let autoria   = document.querySelector('#autoria').value
+function criarCitacao(){
+    let inputData = document.querySelector('#data').value 
+    let citacao = document.querySelector('#citacao').value
+    let autoria = document.querySelector('#autoria').value
 
-    let data = new Date(dataInput)
-    let dataFormatada = data.toLocaleDateString('pt-BR', {timeZone: 'UTC'});
+    let data = new Date(inputData)
+    let dataFormatada = data.toLocaleDateString('pt-BR',{timeZone: 'UTC'})
 
-    // validacao simples
-    if (data == '' || citacao == '' || autoria == '')  {
+    if(data == '' || citacao == '' || autoria == ''){
         abrirModal()
-    } else {
-        criarMensagem(dataFormatada, citacao, autoria)
+    }else{
+        criarMensagem(data, citacao, autoria)
         window.scroll(0,670)
     }
-
 }
-
-function abrirModal() {
-    location.href="#abrirModal"
+function abrirModal(){
+    location.href = '#abrirModal'
 }
-
-function criarMensagem(data, citacao, autoria) {
-    dataCitacao.textContent    = data
-    textoCitacao.textContent   = citacao
+function criarMensagem(data, citacao,autoria){
+    dataCitacao.textContent = data
+    textoCitacao.textContent = citacao
     autoriaCitacao.textContent = autoria
 
     download()
 }
-
-function download() {
+function download(){
     html2canvas(citacao00).then(canvas => {
         document.body.appendChild(canvas)
     })
-
     let aviso2 = document.querySelector('#aviso2')
-    aviso2.textContent = "Clique com o botão direito e escolha Salvar imagem como..."
+    aviso2.textContent = 'Clique com o botão direito para baixar'
 }
-
 const btnLimpar = document.querySelector('#btnLimpar')
 
 btnLimpar.addEventListener('click', () => {
