4a3f6b26441f13006f1ff71da82743094226b5d1
Thiago Costa <tigurrock@gmail.com>
2022-12-12 10:02:46 -0300

    exer

--- exe7/quiz3/script.js
+++ exe7/quiz3/script.js
@@ -32,129 +32,84 @@ let somAplausos = document.querySelector('#somAplausos')
 // ENDERECO DO ARQUIVO JSON
 const url = 'data.json'
 
-function pegarDados(i) {
+function pegarDados(i){
+  fetch(url).then(response => {
+    return response.json()
+  }).then(data => {
+    if(data.erro){
+      console.log('erro ao acessar JSON')
+      return
+    }
+    let qtdDeQuestoes = (data.questoes.length) - 1
+    total.textContent = parseInt(qtdDeQuestoes)
 
-  fetch(url).then(response =>{
-      
-        return response.json();
-
-      }).then(data => {
-
-        if(data.erro) {
-          console.log("Erro ao acessar o JSON")
-          return
-        }
-        
-        // passar o quantidade de questoes para a variavel
-        let qtdQuestoes = (data.questoes.length)-1
-        // escrver a qtdQuestoes para total
-        total.textContent = parseInt(qtdQuestoes)
-        
-        // passe o valor de i no parametro
-        atribuirDados(data, i)
-
-      })
-      
-} // fim pegarDados
-
-function atribuirDados(data, i) {
-  if(i >= data.questoes.length) {
-    //console.log('Fim das questoes')
+    atribuirDados(data, i)
+  })
+}
+function atribuirDados(data, i){
+  if(i >= data.questoes.length){
     i = 1
   }
-    nQuestao.textContent = data.questoes[i].numQuestao
-    pergunta.textContent = data.questoes[i].pergunta
+  nQuestao.textContent = data.questoes[i].numQuestao
+  pergunta.textContent = data.questoes[i].pergunta
+  a.textContent = data.questoes[i].alternativaA
+  b.textContent = data.questoes[i].alternativaB
+  c.textContent = data.questoes[i].alternativaC
+  d.textContent = data.questoes[i].alternativaD
 
-    a.textContent = data.questoes[i].alternativaA
-    b.textContent = data.questoes[i].alternativaB
-    c.textContent = data.questoes[i].alternativaC
-    d.textContent = data.questoes[i].alternativaD
-    
-    numero.textContent = data.questoes[i].numQuestao
-    
-    let certa = document.querySelector('#correct')
-    certa.value = data.questoes[i].correta
-    //console.log(resposta)
+  numero.textContent = data.questoes[i].numQuestao
+  let certa = document.querySelector('#correct')
+  certa.value = data.questoes[i].correta
 }
-
-// COMECAR O QUIZ
 let questaoAtual = 1
 pegarDados(1)
 
-function proximaQuestao(numQuestao) {
+function proximaQuestao(numQuestao){
   let proxima = parseInt(numQuestao)
   pegarDados(proxima)
 }
-
-function verificarSeAcertou(nQuestao, resposta) {
-
+function verificarSeAcertou(nQuestao, resposta){
   let numeroDaQuestao = nQuestao.value
-  //console.log("Questão " + numeroDaQuestao)
-
   let respostaEscolhida = resposta.textContent
-  //console.log("RespU " + respostaEscolhida)
-
-  // usar o fetch para pegar os dados
   pegarDados(numeroDaQuestao)
-
   let respostaCorrect = correct.value
-  //console.log(respostaCorrect)
-
-  if(respostaEscolhida == respostaCorrect) {
-      //console.log("Acertou")
-      somAcerto.play()      
-      pontos += 10 // pontos = pontos + 10
-  } else {
-      console.log("Errou!")
-      somErro.play()
+  if(respostaCorrect == respostaEscolhida){
+    pontos += 10
+    somAcerto.play()
+  }else{
+    somErro.play()
+    console.log('errou!')
   }
-
   quantidadeDeQuestoes = parseInt(total.textContent)
-  //console.log("Total " + quantidadeDeQuestoes)
-
-  proxima = parseInt(numero.textContent)+1
-  //console.log("Próxima " + proxima)
-
+  proxima = parseInt(numero.textContent) + 1
   setTimeout(function() {
-    
-    if(proxima > quantidadeDeQuestoes) {
-        console.log('Fim do Jogo!')
-        fimDoJogo()
-    } else {
-        proximaQuestao(proxima)
+    if(proxima > quantidadeDeQuestoes){
+      fimDoJogo()
+    }else{
+      proximaQuestao(proxima)
     }
   }, 50)
 
-  // atualizar o placar
   atualizarPlacar()
-
 }
-
-function atualizarPlacar() {
+function atualizarPlacar(){
   placar.textContent = pontos
 }
-
-function fimDoJogo() {
-
+function fimDoJogo(){
   somAplausos.play()
 
   let s = 's'
   pontos == 0 ? s = '' : s = s
-  instrucoes.textContent = "Fim de Jogo! Você conseguiu " + pontos + " ponto"+ s
+  instrucoes.textContent = 'Fim do jogo! Você finalizou com '+pontos+ ' ponto'+s
 
   instrucoes.classList.add('placar')
-
-  // OCULTAR O ARTICLE DA QUESTAO
   questao.style.display = 'none'
-
   setTimeout(function() {
-      pontos = 0 // zerar placar
-      instrucoes.classList.remove('placar')
-
-      // REINICIAR O JOGO
-      questao.style.display = 'block'
-      proximaQuestao(1)
-      instrucoes.textContent = 'Leia a questão e clique na resposta correta'
+    pontos = 0
+    instrucoes.classList.remove('placar')
+    
+    questao.style.display = 'block'
+    proximaQuestao(1)
+    instrucoes.textContent = 'Leia a questão e clique na reposta correta'
   }, 7000)
-
 }
