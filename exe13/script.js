98fa05e0b52f1a4c32a8cca05926aee29ed5c81a
Thiago Costa <tigurrock@gmail.com>
2023-01-19 08:58:05 -0300

    att

--- exe13/script.js
+++ exe13/script.js
@@ -3,9 +3,9 @@
 let banco = []
 
 // armazenar em constante array function que retorna os valores da chave 'todoList'
-const getBanco = () =>  JSON.parse(localStorage.getItem('todoList')) ?? []
+const getBanco = () => JSON.parse(localStorage.getItem('todoList')) ?? []
 // armazenar em constante array function que define um valor da chave 'tarefa' do nosso 'todoList'
-const setBanco = (banco) => localStorage.setItem('todoList', JSON.stringify(banco))
+const setBanco = (banco) => localStorage.setItem('todoList',JSON.stringify(banco))
 
 const criarItem = (tarefa, status, indice) => {
     const item = document.createElement('label')
