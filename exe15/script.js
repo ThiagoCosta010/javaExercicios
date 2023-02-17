f25e6c47ef881cd963ad54001f9a9cf706ab8527
Thiago Costa <tigurrock@gmail.com>
2023-02-16 21:04:58 -0300

    att

--- exe15/script.js
+++ exe15/script.js
@@ -1,25 +1,21 @@
-pizzaJson.map((item, index) => {
-    let pizzaItem = document.querySelector('.models .pizza-item').cloneNode(true)
+const seleciona = (elemento) => document.querySelector(elemento)
+const selecionaTodos = (elemento) => document.querySelectorAll(elemento)
 
-    // console.log(pizzaItem)
-    document.querySelector('.pizza-area').append(pizzaItem)
-
-    pizzaItem.querySelector('.pizza-item--img img').src = item.img
-    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`
-    pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name
-    pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description
-
-    pizzaItem.querySelector('.pizza-item a').addEventListener('click', (e) => {
-        e.preventDefault()
-        document.querySelector('.pizzaWindowArea').style.display = 'flex'
-
-        
-        document.querySelector('.pizzaBig img').src = item.img
-        document.querySelector('.pizzaInfo h1').innerHTML = item.name
-        document.querySelector('.pizzaInfo--desc').innerHTML = item.description
-        document.querySelector('.pizzaInfo--actualPrice').innerHTML = `R$ ${item.price.toFixed(2)}`
-    })
-
+const abrirModal = () => {
+    seleciona('.pizzaWindowArea').style.opacity = 0
+    seleciona('.pizzaWindowArea').style.display = 'flex'
+    setTimeout(() => {
+        seleciona('.pizzaWindowArea').style.opacity = 1
+    }, 150)
+}
+const fecharModal = () => {
+    seleciona('.pizzaWindowArea').style.opacity = 0
+    setTimeout(() => {
+        seleciona('.pizzaWindowArea').style.display = 'none'
+    }, 500)
+}
+const botoesFechar = () => {
+    /*
     document.querySelector('.pizzaInfo--cancelButton').addEventListener('click', () => {
         document.querySelector('.pizzaWindowArea').style.display = 'none'
     })
@@ -27,4 +23,46 @@ pizzaJson.map((item, index) => {
     document.querySelector('.pizzaInfo--cancelMobileButton').addEventListener('click', () => {
         document.querySelector('.pizzaWindowArea').style.display = 'none'
     })
+    */
+    selecionaTodos('.pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton').forEach((item) => {
+        item.addEventListener('click', fecharModal)
+    })
+}
+const preencheDadosDasPizzas = (pizzaItem, item) => {   
+    pizzaItem.querySelector('.pizza-item--img img').src = item.img
+    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`
+    pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name
+    pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description
+}
+const preencheDadosModal = (item) => {
+    /*
+    document.querySelector('.pizzaWindowArea').style.display = 'flex'
+    document.querySelector('.pizzaBig img').src = item.img
+    document.querySelector('.pizzaInfo h1').innerHTML = item.name
+    document.querySelector('.pizzaInfo--desc').innerHTML = item.description
+    document.querySelector('.pizzaInfo--actualPrice').innerHTML = `R$ ${item.price.toFixed(2)}`
+    */
+    seleciona('.pizzaBig img').src = item.img
+    seleciona('.pizzaInfo h1').innerHTML = item.name
+    seleciona('.pizzaInfo--desc').innerHTML = item.description
+    seleciona('.pizzaInfo--actualPrice').innerHTML = `R$  ${item.price.toFixed(2)}`     
+}
+pizzaJson.map((item, index) => {
+    let pizzaItem = document.querySelector('.models .pizza-item').cloneNode(true)
+
+    // console.log(pizzaItem)
+    //document.querySelector('.pizza-area').append(pizzaItem)
+    seleciona('.pizza-area').append(pizzaItem)
+
+    preencheDadosDasPizzas(pizzaItem, item)
+
+    pizzaItem.querySelector('.pizza-item a').addEventListener('click', (e) => {
+        e.preventDefault()
+        abrirModal()
+
+        preencheDadosModal(item)
+
+    })
+
+   botoesFechar()
 })
